d1a44dbfde42243c8a94caecdefdda5df731a6fc
匡凡 <kuangfan@dreawer.com>
2019-05-23 17:43:44 +0800

    处理响应data

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { AxiosRequestConfig, AxiosPromise } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import xhr from './xhr'
 import { bulidURL } from './helpers/url'
 import { processHeaders } from './helpers/header'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 
 function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  return xhr(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
 }
 
 function processConfig (config: AxiosRequestConfig): void {
@@ -27,6 +29,11 @@ function transformHeaders (config: AxiosRequestConfig): string {
 
 function transformRequestData (config: AxiosRequestConfig): any {
   return transformRequest(config.data)
+}
+
+function transformResponseData (res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
 }
 
 export default axios
